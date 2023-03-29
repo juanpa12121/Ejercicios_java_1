03598fe235e20289349b0c69ca328f3713ec5b80
2616479 <2616479@tcs.com>
2023-03-29 11:58:09 -0500

    Fix issues

---
+++ src/main/java/org/example/MainCuenta.java
@@ -0,0 +1,57 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class MainCuenta {
+
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int option = 0;
+        boolean continuar = true;
+
+        Cuenta cuenta1 = new Cuenta("Pedro");
+
+        do {
+            System.out.println("\nMENU");
+            System.out.println("1. Ingresar");
+            System.out.println("2. Retirar");
+            System.out.println("3. Salir");
+            System.out.println("Seleccione una opción");
+            option = scanner.nextInt();
+
+            switch (option) {
+                case 1:
+                    System.out.println("Tu saldo actual es: "+cuenta1.getCantidad());
+                    System.out.println("Digite cantidad a ingresar: ");
+                    double cantidadIngresar = scanner.nextDouble();
+                    cuenta1.ingresar(cantidadIngresar);
+                    System.out.println(cuenta1.toString());
+                    break;
+                case 2:
+                    System.out.println("Tu saldo actual es: "+cuenta1.getCantidad());
+                    if(cuenta1.getCantidad() == 0){
+                        System.out.println("Sin saldo para retiro");
+                        break;
+                    }
+                    System.out.println("Digite cantidad a retirar: ");
+                    double cantidadRetirar = scanner.nextDouble();
+                    cuenta1.retirar(cantidadRetirar);
+                    System.out.println(cuenta1.toString());
+                    break;
+                case 3:
+                    continuar = false;
+                    System.out.println("Bancolombia le pone el alma, hasta luego.");
+                    break;
+                default:
+                    System.out.println("Opcion invalida");
+                    break;
+            }
+        } while (continuar);
+
+        //Cuenta cuenta1 = new Cuenta("Juan Felipe", 5000);
+        //Cuenta cuenta2 = new Cuenta("Javier", 1000);
+
+    }
+}
